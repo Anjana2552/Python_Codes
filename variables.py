4aaf53a7aa3194458818366c08f563a081c31b51
Anjana2552 <anjanaviswanath9910@gmail.com>
2025-03-22 15:43:48 +0530

    python variables

---
+++ variables.py
@@ -0,0 +1,32 @@
+#python Variables
+#no command for declaring a variable
+x = 5
+y = "Python"
+print(x)
+print(y)
+
+#do not need to be declared with any particular type
+x = 4       # x is of type int
+x = "Variables" # x is now of type str
+print(x)
+
+#specify the data type of a variable-casting
+x = str(3)    # String
+y = int(3)    # integer
+z = float(3)  # floating
+print(x)
+print(y)
+print(z)
+
+#data type of a variable - type() function
+x = 5
+y = "John"
+print(type(x))
+print(type(y))
+
+#assign values to multiple variables in one line
+x, y, z = "Orange", "Banana", "Cherry"
+print(x)
+print(y)
+print(z)
+
