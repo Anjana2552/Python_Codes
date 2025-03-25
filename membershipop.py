1ec5fa1f7ce414f00cf1af823c4dcf9b6f9933ed
Anjana2552 <anjanaviswanath9910@gmail.com>
2025-03-25 10:06:08 +0530

    Membership operator

---
+++ membershipop.py
@@ -0,0 +1,12 @@
+#used to test if a sequence in presented in an object
+
+#IN -> returns TRUE if a sequence with the specified value is present in the object
+
+x=["apple","banana"]
+print("banana" in x)
+
+#NOT IN -> returns TRUE if a sequence with the specified value is not present in the object
+
+print("pineapple" not in x)
+
+print("apple" not in x)
