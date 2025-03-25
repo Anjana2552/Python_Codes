2886be4a029c220517b59dba708b22ef7f064c25
Anjana2552 <anjanaviswanath9910@gmail.com>
2025-03-25 10:37:31 +0530

    Logical operator

---
+++ logicalop.py
@@ -0,0 +1,12 @@
+#AND ->returns TRUE if both statements are TRUE
+
+x=5
+print(x>3 and x<10)
+
+#OR ->returns TRUE if one of the statements is TRUE
+
+print(x>3 or x<4)
+
+#NOT ->reverse the result,returns FALSE if the result is TRUE
+
+print(not(x>3 and x<10))
